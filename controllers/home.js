787a30e4002a584d8252d29f4a43bda2873bf199
Riley Miller <rileymiller@csm-wl-dhcp-193-217.mines.edu>
2018-04-21 10:48:56 -0600

    structured getRecentItemss and are sending data to the client in common.pug

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,14 @@
 //const itemCtrl = require('../api/controllers/itemCtrl.js');
 var mongoose = require('mongoose');
 const User = require('../models/User');
+var request = require('request');
+var apiOptions = {
+    server: "http://localhost:8080"
+};
 
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://nameless-basin-42853.herokuapp.com";
+}
 
 
 /**
@@ -25,23 +32,60 @@ exports.index = (req, res) => {
 exports.common = (req,res) => {
 	console.log('inside common in home.js');
 	console.log(req.params.userid);
-
-	res.render('common', {
-	    title: 'common'
-	  });
+	getRecentItems(req, res, function(req, res, responseData) {
+		renderCommonArea(req, res, responseData);
+	})
+	
 };
+
+
+
+var renderCommonArea = (req,res, responseData) => {
+	console.log('inside renderCommonArea');
+	console.log(responseData);
+	res.render('common', {
+	    title: 'common',
+	    items: responseData
+	  });
+}
 
 var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
 
+
+
+
+var getRecentItems = (req, res, callback) => {
+	var requestOptions, path;
+	console.log('inside getRecentItems');
+    path = "/usersanditems/recentItems";
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {}
+    };
+    request(
+        requestOptions,
+        function(err, response, body) {
+            var data = body;
+            if (response.statusCode === 200) {
+
+                callback(req, res, data);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
+        }
+    );
+}
+
 /**
 * GET /recentItems
 * get the most recent items
 */
 exports.recentItems = (req,res) => {
-	console.log('getting recent items')
+	console.log('getting recent items in exports')
 	items = User.aggregate(
 		[{ $unwind: "$sale" },
 		{ $project: {email: 1, _id: 1, item_id: "$sale._id", item_name: "$sale.name", item_category: "$sale.category", item_price: "$sale.price", item_create: "$sale.created"} },
