6b1787b5494189b1c780a905824f09299d2f1ce8
Riley Miller <rileymiller@csm-wl-dhcp-193-217.mines.edu>
2018-04-21 11:55:28 -0600

    fixed ajax call and server path to retrieve recent items

--- public/js/common.js
+++ public/js/common.js
@@ -1,25 +1,27 @@
 $(document).ready(() => {
 
 	console.log('inside common.js');
-
-
-	var apiOptions = {
+	
+	/*var apiOptions = {
 	    server: "http://localhost:8080"
-	};
+	};*/
 
-	if (process.env.NODE_ENV === 'production') {
-	    apiOptions.server = "https://nameless-basin-42853.herokuapp.com";
-	}
-	var items = [];
+	var server = window.location.origin;
 
+
+	// if (process.env.NODE_ENV === 'production') {
+	//     apiOptions.server = "https://nameless-basin-42853.herokuapp.com";
+	// }
+	 var items = [];
+	 console.log(window.location.origin);
 	$(function(){
 		path = "/usersanditems/recentItems";
 		console.log('inside of ajax')
 		$.ajax({
 			type:'GET',
-			data: JSON.stringify(data),
+			data: JSON.stringify(items),
 			contentType: 'application/json',
-	        url: apiOptions.server + path,						
+	        url: server + path,						
 	        success: function(data) {
 	            console.log('success');
 	                console.log(JSON.stringify(data));
