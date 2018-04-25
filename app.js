f7029588a7a16943112b02d6042c3f4f875a33a6
roy <royprocell@gmail.com>
2018-04-24 18:58:25 -0600

    profile and item pages, no break pls

--- app.js
+++ app.js
@@ -130,6 +130,8 @@ app.use('/usersanditems', routeApi);
 app.get('/', homeController.index);
 app.get('/common/:userid', homeController.common)
 app.get('/newcommon', homeController.newcommon)
+app.get('/:userid/item/:itemid', homeController.item);
+app.get('/:userid/profile', homeController.profile);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
--- controllers/home.js
+++ controllers/home.js
@@ -33,6 +33,24 @@ exports.newcommon = (req,res) => {
       });
 }
 
+exports.profile = (req, res) => {
+	console.log('inside profile');
+	console.log(req.params.userid);
+	
+	getUser(req, res, function(req, res, responseData) {
+		renderProfile(req, res, responseData);
+	});
+};
+
+var renderProfile = (req,res, responseData) => {
+	console.log('inside renderProfile');
+	console.log(responseData);
+	res.render('profile', {
+	    title: 'profile',
+	    p_user: responseData
+	  });
+}
+
 /**
 * GET /:userid
 * Common Page
@@ -81,6 +99,56 @@ var renderCommonArea = (req,res, responseData) => {
 	    items: responseData
 	  });
 }
+
+/**
+* GET /:itemid
+*
+*/
+exports.item = (req,res) => {
+	console.log('inside item');
+	console.log(req.params.itemid);
+	console.log(req.params.userid);
+
+	getSingleItem(req, res, function(req, res, responseData) {
+		renderItem(req, res, responseData);
+	});
+
+
+	
+};
+
+var getSingleItem = (req, res, callback) => {
+	var requestOptions, path;
+	console.log('inside getSingleItem');
+    path = "/usersanditems/item/" + req.params.userid + "/" + req.params.itemid;
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {}
+    };
+    request(
+        requestOptions,
+        function(err, response, body) {
+            var data = body;
+            if (response.statusCode === 200) {
+
+                callback(req, res, data);
+            } else {
+                _showError(req, res, response.statusCode);
+            }
+        }
+    );
+};
+
+var renderItem = (req,res, responseData) => {
+	console.log('inside renderItem');
+	console.log(responseData);
+	res.render('item', {
+	    title: 'item',
+	    item: responseData
+	  });
+}
+
 
 var sendJsonResponse = function(res, status, content) {
     res.status(status);
